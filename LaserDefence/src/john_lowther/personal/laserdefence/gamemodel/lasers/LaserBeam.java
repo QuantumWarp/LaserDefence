027b63f6ae1cd83f4fad4d52a33449cda3fbdcac
John Lowther <jw.lowther@hotmail.co.uk>
2014-02-13 21:48:59 +0000

    Last commit was a lie, here are the lasers!

---
+++ LaserDefence/src/john_lowther/personal/laserdefence/gamemodel/lasers/LaserBeam.java
@@ -0,0 +1,11 @@
+package john_lowther.personal.laserdefence.gamemodel.lasers;
+
+import john_lowther.personal.laserdefence.gamemodel.interfaces.*;
+
+/**
+ * Object representing a single laser beam.
+ * @author John Lowther
+ */
+public abstract class LaserBeam implements Drawable, Expirable {
+	
+}
---
+++ LaserDefence/src/john_lowther/personal/laserdefence/gamemodel/lasers/LaserTargetReticle.java
@@ -0,0 +1,9 @@
+package john_lowther.personal.laserdefence.gamemodel.lasers;
+
+/**
+ * Object for the target. Used on firing and when charging.
+ * @author John Lowther
+ */
+public class LaserTargetReticle {
+
+}
